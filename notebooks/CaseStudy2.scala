58d23bd8353317641e037907769cceaf3685523f
Rahul Sharma <rahul.developer@gmail.com>
2020-01-22 16:34:22 +0530

    Revision

--- notebooks/CaseStudy2.scala
+++ notebooks/CaseStudy2.scala
@@ -21,3 +21,146 @@ dbutils.fs.mount(
 
 // COMMAND ----------
 
+import java.io._
+import java.net._
+import java.util._
+import javax.net.ssl.HttpsURLConnection
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import scala.util.parsing.json._
+
+case class Language(documents: Array[LanguageDocuments], errors: Array[Any]) extends Serializable
+case class LanguageDocuments(id: String, detectedLanguages: Array[DetectedLanguages]) extends Serializable
+case class DetectedLanguages(name: String, iso6391Name: String, score: Double) extends Serializable
+
+case class Sentiment(documents: Array[SentimentDocuments], errors: Array[Any]) extends Serializable
+case class SentimentDocuments(id: String, score: Double) extends Serializable
+
+case class RequestToTextApi(documents: Array[RequestToTextApiDocument]) extends Serializable
+case class RequestToTextApiDocument(id: String, text: String, var language: String = "") extends Serializable
+
+object SentimentDetector extends Serializable {
+    val accessKey = "61da2b5db92f4769a90a5d4cda42f0fb"
+    val host = "https://radtextanalytics.cognitiveservices.azure.com/"
+    val languagesPath = "/text/analytics/v2.1/languages"
+    val sentimentPath = "/text/analytics/v2.1/sentiment"
+    val languagesUrl = new URL(host+languagesPath)
+    val sentimenUrl = new URL(host+sentimentPath)
+    val g = new Gson
+
+    def getConnection(path: URL): HttpsURLConnection = {
+        val connection = path.openConnection().asInstanceOf[HttpsURLConnection]
+        connection.setRequestMethod("POST")
+        connection.setRequestProperty("Content-Type", "text/json")
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", accessKey)
+        connection.setDoOutput(true)
+        return connection
+    }
+
+    def prettify (json_text: String): String = {
+        val parser = new JsonParser()
+        val json = parser.parse(json_text).getAsJsonObject()
+        val gson = new GsonBuilder().setPrettyPrinting().create()
+        return gson.toJson(json)
+    }
+
+    def processUsingApi(request: RequestToTextApi, path: URL): String = {
+        val requestToJson = g.toJson(request)
+        val encoded_text = requestToJson.getBytes("UTF-8")
+        val connection = getConnection(path)
+        val wr = new DataOutputStream(connection.getOutputStream())
+        wr.write(encoded_text, 0, encoded_text.length)
+        wr.flush()
+        wr.close()
+
+        val response = new StringBuilder()
+        val in = new BufferedReader(new InputStreamReader(connection.getInputStream()))
+        var line = in.readLine()
+        while (line != null) {
+            response.append(line)
+            line = in.readLine()
+        }
+        in.close()
+        return response.toString()
+    }
+
+    def getLanguage (inputDocs: RequestToTextApi): Option[Language] = {
+        try {
+            val response = processUsingApi(inputDocs, languagesUrl)
+            val niceResponse = prettify(response)
+            val language = g.fromJson(niceResponse, classOf[Language])
+            if (language.documents(0).detectedLanguages(0).iso6391Name == "(Unknown)")
+                return None
+            return Some(language)
+        } catch {
+            case e: Exception => return None
+        }
+    }
+
+    def getSentiment (inputDocs: RequestToTextApi): Option[Sentiment] = {
+        try {
+            val response = processUsingApi(inputDocs, sentimenUrl)
+            val niceResponse = prettify(response)
+            val sentiment = g.fromJson(niceResponse, classOf[Sentiment])
+            return Some(sentiment)
+        } catch {
+            case e: Exception => return None
+        }
+    }
+}
+
+val toSentiment = (textContent: String) =>
+        {
+            val inputObject = new RequestToTextApi(Array(new RequestToTextApiDocument(textContent, textContent)))
+            val detectedLanguage = SentimentDetector.getLanguage(inputObject)
+            detectedLanguage match {
+                case Some(language) =>
+                    if(language.documents.size > 0) {
+                        inputObject.documents(0).language = language.documents(0).detectedLanguages(0).iso6391Name
+                        val sentimentDetected = SentimentDetector.getSentiment(inputObject)
+                        sentimentDetected match {
+                            case Some(sentiment) => {
+                                if(sentiment.documents.size > 0) {
+                                    sentiment.documents(0).score.toString()
+                                }
+                                else {
+                                    "Error happened when getting sentiment: " + sentiment.errors(0).toString
+                                }
+                            }
+                            case None => "Couldn't detect sentiment"
+                        }
+                    }
+                    else {
+                        "Error happened when getting language" + language.errors(0).toString
+                    }
+                case None => "Couldn't detect language"
+            }
+        }
+
+
+// COMMAND ----------
+
+print(toSentiment("Good Product!"))
+
+// COMMAND ----------
+
+spark.udf.register("toSentiment", toSentiment)
+
+// COMMAND ----------
+
+val data = Seq(("C10001", "Awesome Product"), ("C10002", "Awful Products I have purchased this week!"), ("C10003", "Best Purchase")).toDF("customerid", "remarks")
+
+data.createOrReplaceTempView("customer_remarks")
+
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC 
+// MAGIC SELECT customerid, remarks, toSentiment(remarks) as score
+// MAGIC FROM customer_remarks
+
+// COMMAND ----------
+
